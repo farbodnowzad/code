6f104b742e62844e63533669c4b8c489ab7dfe26
farbodnowzad <farbodn@berkeley.edu>
2017-04-25 18:47:34 -0700

    Create ripple_websocket.js

---
+++ ripple_websocket.js
@@ -0,0 +1,21 @@
+const websocket = require('ws');
+const ws = new websocket("wss://s1.ripple.com:443");
+ws.subscribe
+ws.on("open", function(){
+    var d = new Date()
+    console.log("websocket opened at " + d.getHours() + ":" 
+        + d.getMinutes() + ":" + d.getSeconds())
+    var subscription = {
+    "id": "Example watch for new validated ledgers",
+    "command": "subscribe",
+    "streams": ["ledger"]
+    }
+    ws.send(JSON.stringify(subscription))
+})
+ws.on("error", function(error){
+    console.log(error)
+})
+ws.on("message", function incoming(data) {
+    var obj = JSON.parse(data)
+    console.log(obj)
+});
